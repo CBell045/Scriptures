5b690276d5b7e1da03fe88805288c70d399c1871
Chad <chadbell045@gmail.com>
2024-08-06 07:58:49 -0700

    added heart mode

--- pages/1_Generator.py
+++ pages/1_Generator.py
@@ -5,6 +5,8 @@ from streamlit_extras.stoggle import stoggle
 
 scriptures = pd.read_parquet("./data/scriptures.parquet")
 
+heart_scriptures = scriptures['scripture_text'].str.contains("heart", case=False)
+
 if 'show_scripture' not in st.session_state:
     st.session_state.show_scripture = False
 
@@ -21,11 +23,13 @@ def show_scripture():
 def show_reference():
     st.session_state.show_reference = True
 
-def generate_scripture(volume=None):
+def generate_scripture(_volume, _heart_mode):
+    if _heart_mode:
+        _scriptures = scriptures[heart_scriptures]
     if volume is None or volume == "Any":
-        return scriptures.sample()
+        return _scriptures.sample()
     else:
-        return scriptures[scriptures.volume_title == volume].sample()
+        return _scriptures[_scriptures.volume_title == _volume].sample()
 
 
 
@@ -33,17 +37,17 @@ st.sidebar.title("Options")
 st.sidebar.subheader("Select a volume of scripture")
 options = np.append("Any", scriptures.volume_title.unique())
 volume = st.sidebar.selectbox("Volume", options, index=3)
-    
+
 
 
 st.title("Random Scripture Generator")
 st.subheader("Click the button below to generate a random scripture")
 
 
-
+heart_mode = st.toggle("Heart Mode", True)
 
 if st.button("Generate", on_click=show_scripture): 
-    scripture = generate_scripture(volume)
+    scripture = generate_scripture(volume, heart_mode)
     st.session_state.scripture = scripture
     st.write(scripture['scripture_text'].values[0])
     stoggle(
