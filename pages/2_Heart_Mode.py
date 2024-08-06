d26b683c2fd2a3c822bb180e3a32a175c8d6ebbc
Chad <chadbell045@gmail.com>
2024-08-06 08:11:03 -0700

    added heart talks and scriptures page

---
+++ pages/2_Heart_Mode.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.markdown("""
+# Heart Mode
+This page is dedicated to scriptures and talks about the heart.
+            
+## Heart Talks
+
+            
+- [Consider the Goodness and Greatness of God](https://www.churchofjesuschrist.org/study/general-conference/2020/04/26renlund?lang=eng) (Elder Renlund)
+- [Preserving the Heart’s Mighty Change](https://www.churchofjesuschrist.org/study/general-conference/2009/10/preserving-the-hearts-mighty-change?lang=eng) (Elder Renlund)
+
+## Heart Scriptures
+- [Alma 5:14](https://www.churchofjesuschrist.org/study/scriptures/bofm/alma/5?lang=eng&id=14#p14) And now behold, I ask of you, my brethren of the church, have ye spiritually been born of God? Have ye received his image in your countenances? Have ye experienced this mighty change in your hearts? 
+- [Guide to the Scriptures](https://www.churchofjesuschrist.org/study/scriptures/gs/heart?lang=eng)  
+- [Topical Guide](https://www.churchofjesuschrist.org/study/scriptures/tg/heart?lang=eng)  
+"""
+)
